e21a8ca30a341df5da419d766f66df6c2b58c53b
Nexian32 <dhazzelio.windows@gmail.com>
2021-10-20 17:54:29 +0700

    menu

---
+++ kasir.py
@@ -0,0 +1,21 @@
+from tkinter import *
+
+root = Tk()
+root.title('menu')
+root.geometry("200x700")
+
+Label(root, text='1 = rendang + nasi').pack()
+Label(root, text='2 = ayam pop + nasi').pack()
+Label(root, text='3 = tahu + telor + nasi').pack()
+Label(root, text='4 = sop sapi + nasi').pack()
+Label(root, text='5 = tongkol asam padeh').pack()
+Label(root, text='6 = indomie + telor').pack()
+Label(root, text='7 = indomie kuah keju').pack()
+Label(root, text='8 = indomie kuah keju + telor').pack()
+Label(root, text='9 = roti bakar coklat susu').pack
+Label(root, text='10 = roti bakar coklat keju').pack()
+Label(root, text='11 = roti bakar keju susu').pack()
+Label(root, text='12 = roti bakar telur keju').pack()
+
+
+root.mainloop()
